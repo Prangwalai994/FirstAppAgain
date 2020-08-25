cc39095a86b277096e85c533db826f569eaebf38
Wae <pangwalai.li@rmuti.ac.th>
2020-08-25 16:59:28 +0700

    Add File

---
+++ app/src/main/java/com/example/firstappagain/Fragment/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.firstappagain.Fragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.firstappagain.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
